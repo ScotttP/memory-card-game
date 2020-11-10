0d1be2d4367523614ef991c637e9db4eb75af4e6
Scott Pribe <Scottpribe@gmail.com>
2020-11-09 21:33:06 -0500

    figured out the shuffle array function

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,27 @@
 import React, { useState } from "react";
 
 export default function Card() {
-	return <h1>MEMORY CARD</h1>;
+	const [cardArray, setCardArray] = useState([
+		{ image: "https://...", name: "Harry Potter", clickedOn: false },
+		{ image: "https://...", name: "Hermoine Grainger", clickedOn: false },
+		{ image: "https://...", name: "Ron Weasley", clickedOn: false },
+		{ image: "https://...", name: "Neville Longbottom", clickedOn: false },
+	]);
+
+	function shuffleIndex() {
+		let newArray = [];
+		for (let i = 0; i <= cardArray.length - 1; i++) {
+			let newIndex = Math.floor(Math.random() * cardArray.length);
+			newArray.splice(newIndex, 0, cardArray[i]);
+		}
+		setCardArray(newArray);
+		console.log(cardArray);
+	}
+
+	return (
+		<div>
+			<h1>MEMORY CARD</h1>
+			<button onClick={shuffleIndex}>shuffle</button>
+		</div>
+	);
 }
