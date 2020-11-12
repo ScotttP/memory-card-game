aa09a6b284c21350eda06bbe0a0fc59398160fa8
Scott Pribe <Scottpribe@gmail.com>
2020-11-11 22:34:16 -0500

    refactoring my code

--- src/App.js
+++ src/App.js
@@ -3,67 +3,54 @@ import Card from "./components/card";
 import Scoreboard from "./components/scoreboard";
 
 import "./App.css";
+import cardArrayData from "./components/cardArray";
 
 export default function App() {
-	const [cardArray, setCardArray] = useState([
-		{ image: "https://...", name: "Harry Potter", clickedOn: false },
-		{ image: "https://...", name: "Hermoine Grainger", clickedOn: false },
-		{ image: "https://...", name: "Ron Weasley", clickedOn: false },
-		{ image: "https://...", name: "Neville Longbottom", clickedOn: false },
-	]);
+	const shuffleArray = (array) => {
+		let newArray = JSON.parse(JSON.stringify(array));
+		for (let i = newArray.length - 1; i > 0; i--) {
+			let newIndex = Math.floor(Math.random() * (i + 1));
+			let tempArray = newArray[i];
+			newArray[i] = newArray[newIndex];
+			newArray[newIndex] = tempArray;
+		}
+		return newArray;
+	};
+
+	const [cardArray, setCardArray] = useState(() => shuffleArray(cardArrayData));
 	const [currentScore, setCurrentScore] = useState(0);
 	const [highScore, setHighScore] = useState();
 
+	// useEffect(() => {
+	// 	if (!localStorage.getItem("highScore")) {
+	// 		setHighScore(0);
+	// 	} else {
+	// 		setHighScore(JSON.parse(localStorage.getItem("highScore")));
+	// 	}
+	// }, [highScore]);
+
 	useEffect(() => {
-		if (!localStorage.getItem("highScore")) {
-			setHighScore(0);
-		} else {
-			setHighScore(JSON.parse(localStorage.getItem("highScore")));
-		}
-	}, [highScore]);
+		console.log("clicked");
+	});
 
-	function wrapperFunction(index) {
-		shuffleIndex(index); //once this is called, shuffleIndex doesn't work... might have to do with the rendering of the component.
-		//game();
+	function markAsClicked(e) {
+		// if (e.clickedOn) {
+		// 	evaluateHighScore();
+		// 	setCurrentScore(0);
+		// 	setCardArray(cardArrayData);
+		// } else {
+		// 	cardArrayCopy[index].clickedOn = true;
+		// 	shuffleArray(cardArrayCopy);
+		// 	setCurrentScore(currentScore + 1);
+		// }
 	}
 
-	function shuffleIndex(index) {
-		let newArray = [];
-		for (let i = 0; i <= cardArray.length - 1; i++) {
-			let newIndex = Math.floor(Math.random() * cardArray.length);
-			newArray.splice(newIndex, 0, cardArray[i]);
-		}
-		markAsClicked(index, newArray);
-	}
-	function markAsClicked(index, newArray) {
-		if (cardArray[index].clickedOn === true) {
-			evaluateHighScore();
-			setCurrentScore(0);
-			resetGame();
-		} else {
-			cardArray[index].clickedOn = true;
-			setCardArray(newArray);
-			setCurrentScore(currentScore + 1);
-		}
-	}
 	function evaluateHighScore() {
-		console.log();
 		if (!localStorage.getItem("highScore") || currentScore > highScore) {
 			setHighScore(currentScore);
 			localStorage.setItem("highScore", currentScore);
-		} else {
-			setHighScore(JSON.parse(localStorage.getItem("highscore")));
 		}
 		setCurrentScore(0);
-	}
-
-	function resetGame() {
-		setCardArray([
-			{ image: "https://...", name: "Harry Potter", clickedOn: false },
-			{ image: "https://...", name: "Hermoine Grainger", clickedOn: false },
-			{ image: "https://...", name: "Ron Weasley", clickedOn: false },
-			{ image: "https://...", name: "Neville Longbottom", clickedOn: false },
-		]);
 	}
 
 	const memoryCardRendering = cardArray.map((element, index) => (
@@ -71,7 +58,7 @@ export default function App() {
 			key={element.name + index}
 			index={index}
 			info={element}
-			onClick={() => wrapperFunction(index)}
+			onClick={() => markAsClicked()}
 		/>
 	));
 
---
+++ src/components/cardArray.js
@@ -0,0 +1,18 @@
+const cardArrayData = [
+	{ image: "https://...", name: "Harry Potter", clickedOn: false },
+	{ image: "https://...", name: "Hermoine Grainger", clickedOn: false },
+	{ image: "https://...", name: "Ron Weasley", clickedOn: false },
+	{ image: "https://...", name: "Neville Longbottom", clickedOn: false },
+	{ image: "https://...", name: "Rubeus Hagrid", clickedOn: false },
+	{ image: "https://...", name: "Sirius Black", clickedOn: false },
+	{ image: "https://...", name: "Severus Snap", clickedOn: false },
+	{ image: "https://...", name: "Draco Malfoy", clickedOn: false },
+	{ image: "https://...", name: "Albus Dumbledore", clickedOn: false },
+	{ image: "https://...", name: "Voldemort", clickedOn: false },
+	{ image: "https://...", name: "Bellatrix Lestrange", clickedOn: false },
+	{ image: "https://...", name: "Remus Lupin", clickedOn: false },
+	{ image: "https://...", name: "Luna Lovegood", clickedOn: false },
+	{ image: "https://...", name: "Dobby", clickedOn: false },
+];
+
+export default cardArrayData;
