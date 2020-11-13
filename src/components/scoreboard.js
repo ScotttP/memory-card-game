375105e4c28e475d8fe0d7f4379ef5ca6ab81edc
Scott Pribe <Scottpribe@gmail.com>
2020-11-13 15:31:35 -0500

    added some styling. need to make it responsive

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -1,8 +1,14 @@
 export default function Scoreboard(props) {
 	return (
 		<header className="scoreboard">
-			<h1>High Score: {props.highScore}</h1>
-			<h1>Current Score: {props.currentScore}</h1>
+			<img
+				id="memoryCardFont"
+				src="https://fontmeme.com/permalink/201113/348a818b58206d7bcc3eba27604c570b.png"
+			></img>
+			<div id="scoreContainer">
+				<h1>High Score: {props.highScore}</h1>
+				<h1>Current Score: {props.currentScore}</h1>
+			</div>
 		</header>
 	);
 }
